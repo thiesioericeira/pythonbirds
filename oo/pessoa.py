2f640913ed0f29a283ec1e7dc8d062b24b94829f
thiesioericeira <ericeirathiesioadm@hotmail.com>
2021-07-25 20:54:48 -0300

    Criado atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,23 @@
 class Pessoa:
-    def __init__(self,nome = None, idade = 35):
+    def __init__(self,*filhos, nome = None, idade = 35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar (self):
         return f'olá {id(self)}'
         self.nome = none
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Thiesio'
-    print(p.nome)
-    print(p.idade)
-
+    renzo = Pessoa(nome='Renzo')
+    Luciano = Pessoa(renzo, nome='luciano')
+    print(Pessoa.cumprimentar(Luciano))
+    print(id(Luciano))
+    print(Luciano.cumprimentar())
+    print(Luciano.nome)
+    print(Luciano.idade)
+    for filho in Luciano.filhos:
+        print(filho.nome)
+    print(Luciano.filhos)
 
 
 
